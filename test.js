81364be6be898a94b31cc2f3cf70b57ab50dc411
MiloDijkman <55231114+MiloDijkman@users.noreply.github.com>
2020-09-15 10:47:08 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -1,19 +1,15 @@
-
-function Auto(merk, bouwjaar) {
-
-    let brand = merk;
-    let year = bouwjaar;
-
-    this.getBrand = function () {
-        return brand;
-    }
-
-    this.getYear = function () {
-        return year;
-    }
-
-    this.showDetails = function () {
-        console.log('Automerk: ' + brand + ‘ bouwjaar: ' + bouwjaar);
-    }
-
+function Student(id, klas, studentnaam) {
+  let studentNummer = id;
+  let klasNaam = klas;
+  let studentnaam = studentNaam;
 }
+
+let studenten1 = new Student('29147', 'MD2A', 'Milo');
+let studenten2 = new Student('29093', 'MD2A', 'Max');
+let studenten3 = new Student('28746', 'MD2A', 'Romer');
+let studenten4 = new Student('29743', 'MD2A', 'Bram');
+let studenten5 = new Student('12700', 'MD2A', 'Lau');
+let studenten6 = new Student('29253', 'MD2A', 'Teun');
+let studenten7 = new Student('29118', 'MD2A', 'Pepijn');
+let studenten8 = new Student('28462', 'MD2A', 'Daan');
+let studenten9 = new Student('29215', 'MD2A', 'Rick');
